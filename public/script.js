4f90658ada194c55a5c5b48fee2afccf5d8926d9
Buu Chi Mach (Sophie) <139316490+Buuchia@users.noreply.github.com>
2024-04-10 07:24:17 +1000

    new function

--- public/script.js
+++ public/script.js
@@ -7,14 +7,24 @@ cnv.height = innerHeight
 
 const ctx = cnv.getContext (`2d`)
 
+requestAnimationFrame(draw_frame)
+
 const draw_frame = () => {
-   ctx.fillStyle = `turquoise`
+   //draw the background
+   ctx.fillStyle = rand_col ()
    ctx.fillRect (0, 0, innerWidth, innerHeight)
 
+   //draw the square
+   ctx.fillStyle = 'tomato'
+   ctx.fillRect = (x, y, size, size)
+
+   //call the next frame
    requestAnimationFrame (draw_frame)
 }
 
-requestAnimationFrame(draw_frame)
+function rand_col () {
+   return 'hsl(${ Math.random () *360 }, 50%, 50%)'
+}
 
 window.onresize = () => {
    cnv.width = innerWidth
