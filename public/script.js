efd2ec3cd58a754ba1109f6f0927b8e8f70fae8e
Buu Chi Mach (Sophie) <139316490+Buuchia@users.noreply.github.com>
2024-04-10 07:55:10 +1000

    fill rect delete =

--- public/script.js
+++ public/script.js
@@ -11,35 +11,37 @@ ctx.fillRect (0, 0, innerWidth, cnv.height)
 ctx.fillStyle = rand_col ()
 
 
-// function draw_square (size) {
-//    //draw the square
-//    const x = cnv.width / 2 - size / 2
-//    const y = cnv.height / 2 - size / 2
-//    ctx.fillStyle = `turquoise`
-//    ctx.fillRect = (x, y, 50, 50)
-// }
+function draw_square (size) {
 
-// function draw_squares (start_size) {
-//    draw_square (start_size)
 
-//    if (start_size > 0) {
-//       draw_squares (start_size - 20)
-//    }
+   //draw the square
+   const x = cnv.width / 2 - size / 2
+   const y = cnv.height / 2 - size / 2
+   ctx.fillStyle = `turquoise`
+   ctx.fillRect (x, y, 50, 50)
+}
 
-//    //call the next frame
-//    //requestAnimationFrame (draw_squares)
-// }
+function draw_squares (start_size) {
+   draw_square (start_size)
 
-// //requestAnimationFrame(draw_squares)
+   if (start_size > 0) {
+      draw_squares (start_size - 20)
+   }
 
-// function rand_col () {
-//    return 'hsl(${ Math.random () * 360 }, 50%, 50%)'
-// }
+   //call the next frame
+   //requestAnimationFrame (draw_squares)
+}
 
-// window.onresize = () => {
-//    cnv.width = innerWidth
-//    cnv.height = cnv.height   
-// }
+//requestAnimationFrame(draw_squares)
+
+function rand_col () {
+   return 'hsl(${ Math.random () * 360 }, 50%, 50%)'
+}
+
+window.onresize = () => {
+   cnv.width = innerWidth
+   cnv.height = cnv.height   
+}
 
 
 //Link: https://www.youtube.com/watch?v=jPsZwrV9ld0
